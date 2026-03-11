65f0c076f05eda9fbcf038a01dcd1db10e8bab00
Syrine Benali <benalisyrine8@gmail.com>
2026-03-11 12:57:13 +0100

    Add initial project structure and theme setup for FestivalAppMobile

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # festival_app_mobile
+
+
+# Organisation de l'arborescense de la partie front
+
+📁 data : Contiendra tout ce qui touche aux données (locale et distante).
+
+    📁 local : Base de données Room (Offline first), DAO.
+
+    📁 remote : Retrofit, ApiService.
+
+    📁 repository : L'implémentation de vos repositories (qui feront le pont entre local et remote).
+
+📁 domain:
+
+    📁 models : Les classes de données pures (ex: Festival, User).
+
+    📁 repository : Les interfaces de vos repositories (Pour le "D" de SOLID : Inversion de dépendance).
+
+📁 ui :
+
+    📁 screens : Vos écrans Compose (LoginScreen, FestivalListScreen).
+
+    📁 components : Vos éléments réutilisables (Cartes, boutons, AppBar).
+
+    📁 theme : Déjà créé par Android Studio (Couleurs Material Design 3, Typographie).
---
+++ mobile/app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.festivalappmobile"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.festivalappmobile"
+        minSdk = 36
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+    // --- NAVIGATION COMPOSE ---
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
+    // --- VIEWMODEL & LIFECYCLE (Pour le respect de l'UDF et cycle de vie) ---
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2") // Pour collectAsStateWithLifecycle
+
+    // --- RETROFIT & GSON (Couche Data Distante - API) ---
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // --- ROOM (Couche Data Locale - Offline First) ---
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    // Note: Il faudra ajouter le plugin KSP en haut du fichier pour utiliser Room
+    // ksp("androidx.room:room-compiler:$room_version")
+}
